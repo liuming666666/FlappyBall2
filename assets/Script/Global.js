5b227db930db539f9257ee29c61dee5bccd97ddb
lm <l1093015248@163.com>
2018-03-28 20:20:53 +0800

    youhua

--- assets/Script/Global.js
+++
@@ -1,8 +0,0 @@
-cc.director.getPhysicsManager().enabled = true;
-cc.director.getPhysicsManager().gravity = cc.v2(0, -2700);
-/*cc.director.getPhysicsManager().debugDrawFlags = cc.PhysicsManager.DrawBits.e_aabbBit |
-    cc.PhysicsManager.DrawBits.e_pairBit |
-    cc.PhysicsManager.DrawBits.e_centerOfMassBit |
-    cc.PhysicsManager.DrawBits.e_jointBit |
-    cc.PhysicsManager.DrawBits.e_shapeBit
-;*/
